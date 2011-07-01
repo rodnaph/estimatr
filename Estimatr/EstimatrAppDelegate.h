7d4d8355f4923617688cca49d3be4680fc54b9c8
rodnaph <rod@pu-gh.com>
2011-07-01 16:23:54 +0100

    Initial import

--- Estimatr/EstimatrAppDelegate.h
+++ Estimatr/EstimatrAppDelegate.h
@@ -1,21 +1,11 @@
-//
-//  EstimatrAppDelegate.h
-//  Estimatr
-//
-//  Created by rod on 01/07/2011.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
-//
 
 #import <UIKit/UIKit.h>
 
 @class EstimatrViewController;
 
-@interface EstimatrAppDelegate : NSObject <UIApplicationDelegate> {
-
-}
+@interface EstimatrAppDelegate : NSObject <UIApplicationDelegate> {}
 
 @property (nonatomic, retain) IBOutlet UIWindow *window;
-
 @property (nonatomic, retain) IBOutlet EstimatrViewController *viewController;
 
 @end
